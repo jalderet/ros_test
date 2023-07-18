6a6a8afa861211ff31ee01cb2c3aace8c1ad8ad5
jalderet <137326135+jalderet@users.noreply.github.com>
2023-07-18 09:32:29 -0700

    Update default.sh

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec python3 -m "src.my_publisher_node.py"
+dt-exec python3 -m "my_publisher_node.py"
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
